058f38dfb381c4746c145841d98c3f7b611df591
ericbanker12 <ericbanker12@gmail.com>
2019-10-15 16:52:54 -0500

    style FriendForm

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Form, Label, Input, Button, Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap'
 
 import { axiosWithAuth } from '../utils'
 
@@ -27,39 +28,54 @@ export function FriendForm(props) {
         })
     }
 
+    function cancelHandler(e) {
+        e.preventDefault()
+        props.setShow(false)
+    }
+
     return (
-        <form onSubmit={submitHandler}>
-            <label>
-                <h6>Name</h6>
-                <input 
-                    type='text'
-                    name='name'
-                    placeholder='Name'
-                    value={state.name}
-                    onChange={changeHAndler}
-                />
-            </label>
-            <label>
-                <h6>Age</h6>
-                <input 
-                    type='number'
-                    name='age'
-                    placeholder='Age'
-                    value={state.age}
-                    onChange={changeHAndler}
-                />
-            </label>
-            <label>
-                <h6>E-mail</h6>
-                <input 
-                    type='email'
-                    name='email'
-                    placeholder='E-mail'
-                    value={state.email}
-                    onChange={changeHAndler}
-                />
-            </label>
-            <button type='submit'>Submit</button>
-        </form>
+        <Modal isOpen={props.show}>
+            <Form onSubmit={submitHandler}>
+                <ModalHeader>
+                    <h4>Add a Friend</h4>
+                </ModalHeader>
+                <ModalBody>
+                    <Label className='d-block'>
+                        <h6>Name</h6>
+                        <Input 
+                            type='text'
+                            name='name'
+                            placeholder='Name'
+                            value={state.name}
+                            onChange={changeHAndler}
+                        />
+                    </Label>
+                    <Label className='d-block'>
+                        <h6>Age</h6>
+                        <Input 
+                            type='number'
+                            name='age'
+                            placeholder='Age'
+                            value={state.age}
+                            onChange={changeHAndler}
+                        />
+                    </Label>
+                    <Label className='d-block'>
+                        <h6>E-mail</h6>
+                        <Input 
+                            type='email'
+                            name='email'
+                            placeholder='E-mail'
+                            value={state.email}
+                            onChange={changeHAndler}
+                        />
+                    </Label>
+                </ModalBody>
+                <ModalFooter>
+                    <Button type='submit' className='btn-success'>Add friend</Button>
+                    <Button  type='button' className='btn-danger' onClick={cancelHandler}>Cancel</Button>
+                </ModalFooter>
+            </Form>
+        </Modal>
     )
 }
