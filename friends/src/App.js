47b404c179b2ffce5f6844839ef895369cf6b93f
ericbanker12 <ericbanker12@gmail.com>
2019-10-15 14:50:35 -0500

    create LoginForm component

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
 // import './App.css';
 
+import { LoginForm } from './components'
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         {/* Routes go here */}
+        <Route path='/' component={LoginForm} />
       </Switch>
     </BrowserRouter>
   );
---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import axios from 'axios'
+
+export function LoginForm(props) {
+    const [state, setState] = React.useState({
+        username: 'Lambda School',
+        password: 'i<3Lambd4',
+        loading: false,
+        error: '',
+    })
+
+    function changeHandler(e) {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    function submitHandler(e) {
+        e.preventDefault()
+        setState({...state, loading: true})
+        const {username, password} = state
+        axios.post('http://localhost:5000/api/login', {username, password})
+            .then(resp => {
+                console.log(resp)
+                localStorage.setItem('token', resp.data.payload)
+                // props.history.push()
+            })
+            .catch(err => {
+                setState({...state, loading: false, error: err.message})
+            })
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <label>
+                <h6>Username</h6>
+                <input
+                    type='text'
+                    name='username'
+                    placeholder='username'
+                    value={state.username}
+                    onChange={changeHandler}
+                />
+            </label>
+            <label>
+                <h6>Password</h6>
+                <input
+                    type='password'
+                    name='password'
+                    placeholder='password'
+                    value={state.password}
+                    onChange={changeHandler}
+                />
+            </label>
+            <button type='submit' disabled={state.loading}>Log-in</button>
+            <p>{state.error}</p>
+        </form>
+    )
+}
